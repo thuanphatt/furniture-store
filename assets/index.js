a139023d3d9764ba375740b40ddef08816703446
thuanphatt <zelorntp@gmail.com>
2022-09-22 22:31:28 +0700

    done final

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+// const respmenu = document.getElementById("respmenu");
+// const menu_offcanvas = document.getElementById("menu_offcanvas");
+
+// respmenu.onclick = function () {
+//   menu_offcanvas.classList.toggle("active");
+// };
